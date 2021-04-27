cabeb7d96e6e9932954653820a128ca366a60856
今吉恒介 <ikkorokoro@icloud.com>
2021-04-27 23:26:14 +0900

    データ表示

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -1,6 +1,11 @@
 <template>
   <div id="app">
     <p>{{ message }}</p>
+    <ul>
+      <li v-for= 'memo in memos' :key= 'memo.id'>
+        {{memo.title}}: {{memo.desctiption}}
+      </li>
+    </ul>
   </div>
 </template>
 
@@ -9,14 +14,18 @@ import axios from 'axios';
 export default {
   data: function () {
     return {
-      message: "Hello Vue!"
+      memos: 'memos'
     }
+  },
+  /* mounted()はアプリが立ち上がった最初のタイミング(app.vueファイルが読み込まれた時点)で実行される関数を定義 */
+  mounted () {
+    this.setMemo();
   },
   methods: {
     setMemo: function () {
       axios.get('/api/memos')
       .then(response => (
-        this.memos = response
+        this.memos = response.data
       ))
     }
   }
