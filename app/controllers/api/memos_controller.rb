1b0717f838beb64d2ee9f380ea02f60612030313
今吉恒介 <ikkorokoro@icloud.com>
2021-04-28 16:37:08 +0900

    contorooler create 追加

--- app/controllers/api/memos_controller.rb
+++ app/controllers/api/memos_controller.rb
@@ -2,4 +2,18 @@ class Api::MemosController < ApplicationController
   def index
     @memos = Memo.order('created_at DESC')
   end
+
+  def create
+    @memo = Memo.new(memo_params)
+    if @memo.save
+      render :show, status: :created #保存成功
+    else
+      render json: @memo.errors, status: :unprocessable_entity #unprocessable_entity バリデーションエラーの場合に返す
+    end
+  end
+
+  private
+  def memo_params
+  params.permit(:title, :desctiption)
+  end
 end
